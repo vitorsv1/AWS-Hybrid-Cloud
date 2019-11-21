dfcb357673e344f2580b65c920e7d3f84027c2df
vitorsv1 <vitorsv1@al.insper.edu.br>
2019-11-21 17:53:36 -0300

    webserver

---
+++ APS3/main_webserver.py
@@ -0,0 +1,25 @@
+from pymongo import MongoClient
+
+client = MongoClient()
+
+client = MongoClient("mongodb://localhost:27017/")
+
+mydb = client['name_of_database']
+
+mycolletion = mydb['my_table']
+
+json = {
+    'title': 'titulo',
+    'description': 'description',
+    'done': False
+}
+
+#INSERT
+res = mydb.my_table.insert(json) 
+
+#GET
+for i in mydatabase.myTable.find({title: 'MongoDB and Python'}):
+    print(i) 
+
+#COUNT
+print(mydatabase.myTable.count({title: 'MongoDB and Python'})) 
